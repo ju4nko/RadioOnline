c40d9d1a292d272b267e4ef751583973aaf15c57
ju4nko <ju4nko@gmail.com>
2026-07-01 08:36:35 +0200

    Initial Commit

---
+++ RadioOnline/RadioOnlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioOnlineApp.swift
+//  RadioOnline
+//
+//  Created by Juanjo on 01/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct RadioOnlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
